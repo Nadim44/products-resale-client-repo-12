753441e7d86b8c2069dfef85e2e8c7023b724a10
2Nadim <nadimmahamud661@gmail.com>
2022-11-29 03:34:26 +0000

    footer added

--- src/Pages/Shared/ConfirmationModal/ConfirmationModal.js
+++ src/Pages/Shared/ConfirmationModal/ConfirmationModal.js
@@ -22,54 +22,6 @@ const ConfirmationModal = ({ title, message, closeModal, successAction, modalDat
 export default ConfirmationModal;
 
 
-/**
- * 
- * import React from 'react';
-import { Link } from 'react-router-dom';
-
-const Footer = () => {
-    return (
-        <div>
-            this us footer
-            <footer
-                // style={{
-                //     background: `url(${footer})`,
-                //     backgroundSize: 'cover'
-                // }}
-                className='p-10'>
-                <div className="footer">
-                    <div>
-                        <span className="footer-title">Services</span>
-                        <Link to='/' className="link link-hover">Branding</Link>
-                        <Link to='/' className="link link-hover">Design</Link>
-                        <Link to='/' className="link link-hover">Marketing</Link>
-                        <Link to='/' className="link link-hover">Advertisement</Link>
-                    </div>
-                    <div>
-                        <span className="footer-title">Company</span>
-                        <Link to='/' className="link link-hover">About us</Link>
-                        <Link to='/' className="link link-hover">Contact</Link>
-                        <Link to='/' className="link link-hover">Jobs</Link>
-                        <Link to='/' className="link link-hover">Press kit</Link>
-                    </div>
-                    <div>
-                        <span className="footer-title">Legal</span>
-                        <Link to='/' className="link link-hover">Terms of use</Link>
-                        <Link to='/' className="link link-hover">Privacy policy</Link>
-                        <Link to='/' className="link link-hover">Cookie policy</Link>
-                    </div>
-                </div>
-                <div className='text-center mt-24'>
-                    <p>Copyright © 2022 - All right reserved by Nadim Group Of Ltd</p>
-                </div>
-            </footer>
-        </div>
-    );
-};
-
-export default Footer;
- * 
- */
 
 /**
  * import React from 'react';
